c0cb09a5812c3d8a92989bebac03c5afc7c26738
Deoprag <pdroesofiarabelo@gmail.com>
2023-04-10 20:14:46 -0300

    Arrumando DER e entidade Sale

--- numismaster/src/main/java/com/numismaster/model/Sale.java
+++ numismaster/src/main/java/com/numismaster/model/Sale.java
@@ -1,19 +1,14 @@
 package com.numismaster.model;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -28,27 +23,22 @@ import lombok.Setter;
 @AllArgsConstructor
 @Entity(name = "tb_sale")
 public class Sale {
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
 
-    @Column(name = "total_price", nullable = false)
-    private Float totalPrice;
+        @Column(name = "total_price", nullable = false)
+        private Float totalPrice;
 
-    @Column(name = "sale_date", nullable = false, updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date saleDate;
+        @Column(name = "sale_date", nullable = false, updatable = false)
+        @Temporal(TemporalType.TIMESTAMP)
+        private Date saleDate;
 
-    @ManyToMany(cascade = {CascadeType.ALL})
-    @JoinTable(name = "TB_User_Sale",
-            joinColumns = @JoinColumn(name = "sale_id"),
-            inverseJoinColumns = @JoinColumn(name = "user_id"))
-    private List<User> buyers = new ArrayList<>();
+        @ManyToOne(fetch = FetchType.LAZY)
+        @Column(name = "buyer_id", nullable = false)
+        private User buyer;
 
-    @ManyToMany(cascade = {CascadeType.ALL})
-    @JoinTable(name = "TB_User_Sale",
-            joinColumns = @JoinColumn(name = "sale_id"),
-            inverseJoinColumns = @JoinColumn(name = "user_id"))
-    private List<User> sellers = new ArrayList<>();
-
+        @ManyToOne(fetch = FetchType.LAZY)
+        @Column(name = "seller_id", nullable = false)
+        private User seller;
 }
