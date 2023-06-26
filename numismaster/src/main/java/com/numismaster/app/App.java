c028d6d36d51ea322998739f7c631eca34f7837e
Pedro Rocha <pdroesofiarabelo@gmail.com>
2023-06-25 23:23:53 -0300

    Nova classe para iniciar o app - Uma nova classe foi criada com o objetivo de iniciar o programa, já que o "implements Application" não funciona muito bem na hora de transformar o projeto em um .jar

--- numismaster/src/main/java/com/numismaster/app/App.java
+++ numismaster/src/main/java/com/numismaster/app/App.java
@@ -26,4 +26,8 @@ public class App extends Application {
 			e.printStackTrace();
 		}
 	}
+
+	public static void main(String[] args) {
+		launch();
+	}
 }
---
+++ numismaster/src/main/java/com/numismaster/app/Main.java
@@ -0,0 +1,7 @@
+package com.numismaster.app;
+
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
